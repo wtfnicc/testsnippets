a5949b1ead0b0d2447529f6dfb71851b2b2052d4
Niks <Niks@DESKTOP-AODFTG8>
2025-03-30 20:50:45 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,22 @@
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   const num1 = 5;
+//   const num2 = 3;
+  
+//   const result = add(num1, num2);
+//   console.log(`${num1} + ${num2} = ${result}`);
+//This is how the function would look normally
+
+//now we call addfunc and click tab
+function add(a, b) {
+  return a + b;
+}
+
+const num1 = 42;
+const num2 = 2;
+const result = add(num1, num2);
+console.log(`${num1} + ${num2} = ${result}`);
+
+console.log("This is a test for git");
